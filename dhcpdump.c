82be1c0f481044f76f0007ee1bc6a70fb8bd79ca
Boian Bonev <bbonev@ipacct.com>
2023-06-01 20:01:26 +0000

    Add bounds check in printIPaddressCompactMaskAddress

--- dhcpdump.c
+++ dhcpdump.c
@@ -119,17 +119,6 @@ static inline void printIPaddressMask(uint8_t *data) { // {{{ print the data as 
 		data[4],data[5],data[6],data[7]);
 } // }}}
 
-static inline void printIPaddressCompactMaskAddress(uint8_t *data, int len) { // {{{ print the data as series of IP address and masks
-	int i,j;
-	for (i=0;i<len;i++) {
-		j=4+(data[i]+7)/8;
-		printf("%u.%u.%u.%u/%u,%u.%u.%u.%u ",
-			j>4?data[i+1]:0,j>5?data[i+2]:0,j>6?data[i+3]:0,j>7?data[i+4]:0,data[i],
-			data[j-3],data[j-2],data[j-1],data[j]);
-		i=i+j;
-	}
-} // }}}
-
 static inline void print8bits(uint8_t *data) { // {{{ prints a value of 8 bits (1 byte)
 	printf("%u",data[0]);
 } // }}}
@@ -241,6 +230,35 @@ static inline void printReqParmList(uint8_t *data,int len) { // {{{ print the li
 
 	for (i=0;i<len;i++)
 		printf("%-3d (%s)%s%*s",data[i],dhcp_options[data[i]],(i+1<len)?"\n":"",(i+1<len)?44:0,"");
+} // }}}
+
+static inline void printIPaddressCompactMaskAddress(uint8_t *data,int len) { // {{{ print the data as series of IP address and masks
+	int i,j,isok=1;
+
+	if (len<5)
+		isok=0;
+	else
+		for (i=0;i<len;i++) {
+			j=4+(data[i]+7)/8;
+			if (data[i]>32||len<i+j+1) {
+				isok=0;
+				break;
+			}
+			i+=j;
+		}
+	if (!isok) {
+		printf("Malformed\n%44s","");
+		printHex(data,len);
+		return;
+	}
+
+	for (i=0;i<len;i++) {
+		j=4+(data[i]+7)/8;
+		printf("%u.%u.%u.%u/%u,%u.%u.%u.%u ",
+			j>4?data[i+1]:0,j>5?data[i+2]:0,j>6?data[i+3]:0,j>7?data[i+4]:0,data[i],
+			data[j-3],data[j-2],data[j-1],data[j]);
+		i+=j;
+	}
 } // }}}
 
 static inline int printdata(uint8_t *data,int data_len) { // {{{ print the header and the options.
@@ -490,11 +508,11 @@ static inline int printdata(uint8_t *data,int data_len) { // {{{ print the heade
 				break;
 
 			case 93: // Client System Architecture
-                                printf("%d (%s)",data[j+3],data[j+3]>strcountof(client_architecture_names)?"*unknown*":client_architecture_names[data[j+3]]);
-                                break;
+				printf("%d (%s)",data[j+3],data[j+3]>strcountof(client_architecture_names)?"*unknown*":client_architecture_names[data[j+3]]);
+				break;
 
 			case 94: // Client Network Interface Identifier
-				printf("%u (Rev %d.%d)", (data[j+3]<<8|data[j+4]),data[j+3],data[j+4]);
+				printf("%u (Rev %d.%d)",(data[j+3]<<8|data[j+4]),data[j+3],data[j+4]);
 				break;
 
 			case 116: // Auto-Configure
@@ -503,8 +521,8 @@ static inline int printdata(uint8_t *data,int data_len) { // {{{ print the heade
 
 			case 121: // Classless static route
 			case 249: // MSFT - Classless route
-                                printIPaddressCompactMaskAddress(data+j+2,data[j+1]);
-                                break;
+				printIPaddressCompactMaskAddress(data+j+2,data[j+1]);
+				break;
 
 		}
 		printf("\n");
